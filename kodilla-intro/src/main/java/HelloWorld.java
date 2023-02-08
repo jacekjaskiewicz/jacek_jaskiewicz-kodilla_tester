c186119e96b9618f818fff4baa2136103619070a
Jacek Jaskiewicz <testerkodilla2023@gmail.com>
2023-02-08 21:29:58 +0100

    1st commit

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello from FirstClass!");
+        System.out.println("Jacek");
+        System.out.println(33);
+        System.out.println('J');// [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/com/kodilla/MainExtended.java
@@ -0,0 +1,18 @@
+package com.kodilla;
+
+public class MainExtended {
+    public static void main(String[] args) {
+        String tradePair = "BTCUSDT";
+        int timeFrame = 5;
+        double actualPrice = 22799.50;
+        char goLong = 'L';
+        char goShort = 'S';
+        boolean possitionOppened = true;
+        System.out.println(tradePair);
+        System.out.println(timeFrame);
+        System.out.println(actualPrice);
+        System.out.println(goLong);
+        System.out.println(goShort);
+        System.out.println(possitionOppened);
+    }
+}
